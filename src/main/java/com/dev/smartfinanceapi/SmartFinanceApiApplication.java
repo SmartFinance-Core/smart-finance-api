25ffa598ffff1e2b959d06ffdedcf8c8d9844a84
FabricioLop <fabrixxlop@gmail.com>
2026-03-06 01:24:55 -0500

    chore: initial Spring Boot setup

---
+++ src/main/java/com/dev/smartfinanceapi/SmartFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.smartfinanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFinanceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/smartfinanceapi/SmartFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.smartfinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
