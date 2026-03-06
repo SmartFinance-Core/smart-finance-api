012e08d5a5bb582bf442c3226e8b0b5483a33e47
FabricioLop <fabrixxlop@gmail.com>
2026-03-06 01:35:11 -0500

    feat: configure database connection and add User entity

---
+++ src/main/java/com/dev/smartfinanceapi/models/User.java
@@ -0,0 +1,25 @@
+package com.dev.smartfinanceapi.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data // Anotación de Lombok que nos genera getters, setters y constructores invisibles
+@Entity // Le dice a Spring Boot: "Esta clase es una tabla de base de datos"
+@Table(name = "users") // Le ponemos "users" porque "user" a veces da error en SQL
+public class User {
+
+    @Id // Es la llave primaria (Primary Key)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincrementable (1, 2, 3...)
+    private Long id;
+
+    @Column(nullable = false, unique = true) // No puede estar vacío y no pueden haber correos repetidos
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", updatable = false) // Fecha de creación, no se puede modificar luego
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
