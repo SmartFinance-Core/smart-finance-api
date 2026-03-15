494e25c90b7952d591f853935f738416d7c9925a
FabricioLop <fabrixxlop@gmail.com>
2026-03-15 12:21:15 -0500

    fix: repair Flyway migrations before executing them

--- src/main/java/com/dev/smartfinanceapi/config/FlywayConfig.java
+++ src/main/java/com/dev/smartfinanceapi/config/FlywayConfig.java
@@ -11,14 +11,13 @@ public class FlywayConfig {
 
     @Bean
     public Flyway flyway(DataSource dataSource) {
-        // Configuramos Flyway a mano y lo conectamos a nuestro DataSource de MySQL
         Flyway flyway = Flyway.configure()
                 .dataSource(dataSource)
                 .locations("classpath:db/migration")
                 .baselineOnMigrate(true)
                 .load();
 
-        // Le obligamos a ejecutar la migración inmediatamente
+        flyway.repair();
         flyway.migrate();
 
         return flyway;
